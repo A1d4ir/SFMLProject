675cf4dbaaa69e963e23b4fbbd9e7886fde375f2
Aldair Enriquez Duran <aldair_fime@hotmail.com>
2021-10-15 16:20:09 -0500

    Added physics, enemies, rockets and collisions by class in separate files

--- SFMLProject/Source.cpp
+++ SFMLProject/Source.cpp
@@ -1,8 +1,17 @@
 #include "SFML-2.5.1\include\SFML\Graphics.hpp"
+#include <vector>
+#include "Hero.h"
+#include "Enemy.h"
+#include "Rocket.h"
 
 sf::Vector2f viewSize(1024, 768);
 sf::VideoMode vm(viewSize.x, viewSize.y);
 sf::RenderWindow window(vm, "Hello SFMLGame !!!", sf::Style::Default);
+
+void spawnEnemy();
+void shoot();
+
+bool checkCollision(sf::Sprite sprite1, sf::Sprite sprite2);
 
 sf::Texture skyTexture;
 sf::Sprite skySprite;
@@ -10,32 +19,47 @@ sf::Sprite skySprite;
 sf::Texture bgTexture;
 sf::Sprite bgSprite;
 
-sf::Texture heroTexture;
-sf::Sprite heroSprite;
+Hero hero;
 
-sf::Vector2f playerPosition;
-bool playerMoving = false;
+std::vector<Enemy*> enemies;
+std::vector<Rocket*> rockets;
+
+float currentTime;
+float prevTime = 0.0f;
 
 void init(){
 	//Load sky texture
 	skyTexture.loadFromFile("Assets/graphics/sky.png");
 	//Load Background texture
 	bgTexture.loadFromFile("Assets/graphics/bg.png");
-	//Load hero texture
-	heroTexture.loadFromFile("Assets/graphics/hero.png");
 	//Set and Attach Texture to Sprite
 	skySprite.setTexture(skyTexture);
 	bgSprite.setTexture(bgTexture);
-	heroSprite.setTexture(heroTexture);
-	//Hero Position
-	heroSprite.setPosition(sf::Vector2f(viewSize.x / 2, viewSize.y / 2));
-	heroSprite.setOrigin(heroTexture.getSize().x / 2, heroTexture.getSize().y / 2);
+
+	hero.init("Assets/graphics/hero.png", sf::Vector2f(viewSize.x * 0.2f, viewSize.y * 0.5f), 200);
+
+	srand((int)time(0));
+
 }
 
 void draw() {
+	
 	window.draw(skySprite);
 	window.draw(bgSprite);
-	window.draw(heroSprite);
+	window.draw(hero.getSprite());
+
+	for (Enemy* enemy : enemies) {
+
+		window.draw(enemy->getSprite());
+
+	}
+
+	for (Rocket* rocket : rockets) {
+
+		window.draw(rocket->getSprite());
+
+	}
+
 }
 
 void updateInput() {
@@ -44,21 +68,18 @@ void updateInput() {
 
 	while (window.pollEvent(event))
 	{
+
 		if (event.type == sf::Event::KeyPressed) {
 
-			if (event.key.code == sf::Keyboard::Right) {
+			if (event.key.code == sf::Keyboard::Up) {
 
-				playerMoving = true;
+				hero.jump(750.0f);
 
 			}
 
-		}
+			if (event.key.code == sf::Keyboard::Down) {
 
-		if (event.type == sf::Event::KeyReleased) {
-
-			if (event.key.code == sf::Keyboard::Right) {
-
-				playerMoving = false;
+				shoot();
 
 			}
 
@@ -72,8 +93,69 @@ void updateInput() {
 
 void update(float dt) {
 
-	if (playerMoving) {
-		heroSprite.move(50.0f * dt, 0);
+	hero.update(dt);
+	currentTime += dt;
+	//Spawn Enemies
+	if (currentTime >= prevTime + 1.125f) {
+
+		spawnEnemy();
+		prevTime = currentTime;
+
+	}
+
+	//Update Enemies
+	for (int i = 0; i < enemies.size(); i++) {
+
+		Enemy* enemy = enemies[i];
+
+		enemy->update(dt);
+
+		if (enemy->getSprite().getPosition().x < 0) {
+			
+			enemies.erase(enemies.begin() + i);
+			delete(enemy);
+
+		}
+
+	}
+
+	//Update Rockets
+	for (int i = 0; i < rockets.size(); i++) {
+
+		Rocket* rocket = rockets[i];
+		rocket->update(dt);
+
+		if (rocket->getSprite().getPosition().x > viewSize.x) {
+
+			rockets.erase(rockets.begin() + i);
+			delete(rocket);
+
+		}
+
+	}
+
+	//Ckeck Collision Between Rockets and Enemies
+	for (int i = 0; i < rockets.size(); i++) {
+
+		for (int j = 0; j < enemies.size(); j++) {
+
+			Rocket* rocket = rockets[i];
+			Enemy* enemy = enemies[i];
+
+			if (checkCollision(rocket->getSprite(), enemy->getSprite())) {
+
+				rockets.erase(rockets.begin() + i);
+				enemies.erase(enemies.begin() + j);
+
+				delete(rocket);
+				delete(enemy);
+
+				printf(" rocket intersects enemy \n");
+
+			}
+
+		}
+
 	}
 
 }
@@ -81,6 +163,7 @@ void update(float dt) {
 int main(){
 
     sf::Clock clock;
+	window.setFramerateLimit(60);
 
 	//Initialize Game Objects
 	init();
@@ -106,4 +189,58 @@ int main(){
 
 
 	return 0;
+}
+
+void spawnEnemy() {
+
+	int randLoc = rand() % 3;
+
+	sf::Vector2f enemyPos;
+
+	float speed;
+
+	switch (randLoc) {
+	case 0:
+		enemyPos = sf::Vector2f(viewSize.x, viewSize.y * 0.75f);
+		speed = -400;
+		break;
+	case 1:
+		enemyPos = sf::Vector2f(viewSize.x, viewSize.y * 0.60f);
+		speed = -550;
+		break;
+	case 2:
+		enemyPos = sf::Vector2f(viewSize.x, viewSize.y * 0.40f);
+		speed = -650;
+		break;
+	default:
+		printf("Incorrect y value \n");
+		return;
+	}
+
+	Enemy* enemy = new Enemy();
+
+	enemy->init("Assets/graphics/enemy.png", enemyPos, speed);
+
+	enemies.push_back(enemy);
+
+}
+
+void shoot() {
+
+	Rocket* rocket = new Rocket();
+
+	rocket->init("Assets/graphics/rocket.png", hero.getSprite().getPosition(), 400.0f);
+	rockets.push_back(rocket);
+
+}
+
+bool checkCollision(sf::Sprite sprite1, sf::Sprite sprite2) {
+
+	sf::FloatRect shape1 = sprite1.getGlobalBounds();
+	sf::FloatRect shape2 = sprite2.getGlobalBounds();
+
+	if (shape1.intersects(shape2))
+		return true;
+	else
+		return false;
 }
